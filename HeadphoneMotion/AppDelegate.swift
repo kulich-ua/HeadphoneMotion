0be9c3b8cddacb49fb961cb8538b8c9898d0af79
Volodymyr Kolibaba <3177451+kulich-ua@users.noreply.github.com>
2020-12-04 13:55:36 +0100

    Create project using Xcode SceneKit template

---
+++ HeadphoneMotion/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  HeadphoneMotion
+//
+//  Created by Kolibaba, Volodymyr on 04.12.20.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+}
+
